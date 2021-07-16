acc452c9d5cb9ea210d73370488d0e3eefab9c3c
Jaya DeHart <dehartj97@gmail.com>
2021-07-16 12:11:08 -0700

    rework environment variables structure

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ io.on("connection", (socket) => {
 });
 
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL || "mongodb://localhost:27017/tilesDB")
   .then(() => {
     server.listen(process.env.PORT || 5000, () => {
       console.log("listening 5000");
